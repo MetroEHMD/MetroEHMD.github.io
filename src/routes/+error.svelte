69f3db21f5363b7e81cf0877cd343dfefc0069cc
EHMD28 <ehmdiagne0530@gmail.com>
2026-01-17 11:10:39 -0500

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<main>
+	<h1>Error: {page.error.message}</h1>
+	<p>Page Not Found</p>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	main > * {
+		font-size: 3em;
+	}
+</style>
