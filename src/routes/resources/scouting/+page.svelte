3040bda66c302170ef0503dae72223eedcd9a9ca
EHMD28 <ehmdiagne0530@gmail.com>
2026-01-23 14:38:42 -0500

    added todo for scouting

--- src/routes/resources/scouting/+page.svelte
+++ src/routes/resources/scouting/+page.svelte
@@ -1,1 +1,13 @@
-<main></main>
+<main>
+	<p>
+		This needs to be made before competition. See the <a
+			href="https://metrobots.github.io/resources/scouting/">scouting form</a
+		> from last year for an example of what it might look like. It NEEDS to be mobile friendly.
+	</p>
+</main>
+
+<style>
+	main {
+		padding: 2vh 5vw;
+	}
+</style>
